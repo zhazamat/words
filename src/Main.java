e56517e87d42c9121e183125578ec03b80baaecd
zhazamat <azamatkyzyzhanargul@gmail.com>
2022-09-13 19:15:14 +0600

    "Count same letter"

--- src/Main.java
+++ src/Main.java
@@ -13,13 +13,19 @@ public class Main {
     }
     public static void wordInitial(String word){
         String word1=word.toUpperCase();
+
         int count=0;
         int count_words=0;
         char[] w=word1.toCharArray();
-        for(int i=0;i<word1.length();i++){
+        System.out.println(putCharacters());
+        for(int i=1;i<word1.length();i++){
             if(w[i]!=' '&&w[i]!=','&&w[i]!='.'&&w[i]!='!'&&w[i]!=')'&&w[i]!='('&&w[i]!='?'&&w[i]!=';'&&w[i]!='-'&&w[i]!='’'){
                 System.out.print(" "+w[i]);
-                System.out.print( ""+putCharacters().get(w[i])); count++;}
+                System.out.print( ""+putCharacters().get(w[i]));
+                if(putCharacters().get(w[i])==putCharacters().get('D')){
+                    count++;
+                }
+                }
             else {
                 count_words++;
             }
@@ -29,20 +35,7 @@ public class Main {
         System.out.println("   How many words have in this text : "+ count_words);
     }
     public static void main(String[] args) {
-        String word="Dear Harry, Ron wrote to me and told me about his phone call to your Uncle Vernon." +
-                " I do hope you’re all right. I’m on holiday in France at the moment and I didn’t know how " +
-                "I was going to send this to you —what if they’d opened it at customs? —but then Hedwig turned up!" +
-                " I think she wanted to make sure you got something for your birthday for a change." +
-                " I bought your present by owl-order; there wasan advertisement in the Daily Prophet " +
-                "(I’ve been getting it delivered; it’s so good to keep up with what’s going on in the wizarding world)." +
-                " Did you see that picture of Ron and his family a week ago? I bet he’s learning loads." +
-                " I’m really jealous —the ancient Egyptian wizards were fascinating. There’s some interesting " +
-                "local history of witchcraft here, too. I’ve rewritten my whole History of Magic essay to include" +
-                " some of the things I’ve found out, I hope it’s not too long —it’s two rolls of parchment more than " +
-                "Professor Binns asked for. Ron says he’s going to be in London in the last week of the holidays. " +
-                "Can you make it? Will your aunt and uncle let you come? I really hope you can. If not, I’ll see you " +
-                "on the Hogwarts Express on September first! Love from Hermione P.S. Ron says Percy’s Head Boy." +
-                " I’ll bet Percy’s really pleased. Ron doesn’t seem too happy about it";
+        String word="Dear Harry, Ron wrote to me and told me about his phone call to your Uncle Vernon." ;
           wordInitial(word);
     }
 }
